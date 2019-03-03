b6aabaeb83ee48f85a2a560fcb2d09a62d757437
Maria Forsell <sofia@Marias-iMac.fios-router.home>
2019-03-03 14:01:23 -0500

    add templates dir, and an index template

--- polls/views.py
+++ polls/views.py
@@ -5,8 +5,10 @@ from .models import Question
 # Create your views here.
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+	context = {
+		'latest_question_list': latest_question_list
+	}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at questions %s." % question_id)
