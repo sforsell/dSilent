404bacf39a8558f262149a37acf54ae1dad72c58
Maria Forsell <sofia@Marias-iMac.fios-router.home>
2019-03-05 22:56:16 -0500

    add results view and finetune urls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Question
 
 # Create your views here.
@@ -15,8 +17,21 @@ def detail(request, question_id):
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	return HttpResponse("You're looking at the results of question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# redirect after sending form data
+		return HttpResponseRedirect(reverse('polls:results' args=(question.id,)))
 
